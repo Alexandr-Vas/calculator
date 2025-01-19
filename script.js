5b7df800cdf59f5c72dc11547fa4f57103a98c4f
Skaaattt <120054153+Skaaattt@users.noreply.github.com>
2025-01-19 22:27:09 +0300

    implement styles

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const buttons = document.querySelectorAll(".btn");
 
 buttons.forEach((button) => {
     button.addEventListener("click", function(event){
-        let trgInner = event.target.innerHTML;
+        let btnValue = event.target.innerHTML;
 
-        switch (trgInner) {
+        switch (btnValue) {
             case "AC":
                 out.innerHTML= "0"
                 break;
         
             default:
-                if (out.innerHTML === "0" && trgInner !==".") {
-                    out.innerHTML = trgInner;
+                if (out.innerHTML === "0" && btnValue !==".") {
+                    out.innerHTML = btnValue;
                     } else {
-                    out.innerHTML += trgInner
+                    out.innerHTML += btnValue
                     }
         }
     })
