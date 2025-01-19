1b6485537cf5ff3209cfc880faf1af1c641667ac
Skaaattt <120054153+Skaaattt@users.noreply.github.com>
2025-01-20 02:36:23 +0300

    implement operators

--- script.js
+++ script.js
@@ -1,21 +1,72 @@
-const out = document.querySelector(".screen");
+const output = document.querySelector(".calc_output");
 const buttons = document.querySelectorAll(".btn");
+
+let arraySolutionChars = [];
+
+let equation = [];
 
 buttons.forEach((button) => {
     button.addEventListener("click", function(event){
         let btnValue = event.target.innerHTML;
 
+        function changeOperatorInOutput(arraySolution){
+            let last_item = arraySolution[arraySolution.length - 1];
+        
+                if(["/", "*", "+", "-"].includes(last_item)) {
+                    arraySolution.pop();
+                    arraySolution.push(btnValue);
+                    output.value = arraySolution.join("");
+                } else {
+                    arraySolution.push(btnValue);
+                    output.value = arraySolution.join("");
+                }
+        }
+
         switch (btnValue) {
             case "AC":
-                out.innerHTML= "0"
+                output.value= "0";
+                arraySolutionChars = [];
                 break;
-        
+
+            case "+/-":
+                output.value = parseFloat(output.value) * -1;
+                break;
+
+            case "%":
+                output.value= parseFloat(output.value) / 100;
+                break;
+
+            case "/":
+                changeOperatorInOutput(arraySolutionChars)
+                break;
+
+            case "*":
+                changeOperatorInOutput(arraySolutionChars);
+                break;
+            
+            case "-":
+                changeOperatorInOutput(arraySolutionChars);
+                break;
+
+            case "+":
+                changeOperatorInOutput(arraySolutionChars);
+                break;
+
+            case "=":
+                result = eval(output.value);
+                
+                break;            
+
+
+            
+
             default:
-                if (out.innerHTML === "0" && btnValue !==".") {
-                    out.innerHTML = btnValue;
+                if (output.value === "0" && btnValue !==".") {
+                    output.value = btnValue;
                     } else {
-                    out.innerHTML += btnValue
+                    output.value += btnValue
                     }
+                arraySolutionChars.push(btnValue);    
         }
     })
 });
